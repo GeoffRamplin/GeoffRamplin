737fa7614853b898be4c4acfae2aab127a29dfff
Geoff Ramplin <g.ramplin@gmail.com>
2024-03-30 11:37:55 +0000

    Changed the code in the game

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,39 +1,44 @@
 # This program will take 2 random numbers from the user
 # and the computer will select a random number.
 # The user can guess the number.
-
 import random
+# Define global variables
+num_1 = 0
+num_2 = 0
 
-condition = True
-print("Welcome to the number guessing game.")
-print("Type 2 numbers at the prompt and begin guessing the computer's chosen number.")
-print("Good luck!")
-# While loop for main game
-while condition:
-    print("Type a range of numbers and guess a randomly selected number.")
-    try:
-        num_1 = int(input("Enter your first number: "))
-    except ValueError:
-        print("You must enter a number!")
-        continue
-    try:
-        num_2 = int(input("Enter your second number: "))
-    except ValueError:
-        print("You must enter a number!")
-        continue
-    if num_1 == num_2:
-        print("You must select a different number")
+
+# Number selection function
+def num_selection():
+    global num_1, num_2
+    while True:
+        print("Type a range of numbers and guess a randomly selected number.")
+        try:
+            num_1 = int(input("Enter your first number: "))
+        except ValueError:
+            print("You must enter a number!")
+            continue
         try:
             num_2 = int(input("Enter your second number: "))
         except ValueError:
             print("You must enter a number!")
             continue
-    else:
-        continue
+        if num_1 == num_2:
+            print("You must select a different number")
+            try:
+                num_2 = int(input("Enter your second number: "))
+            except ValueError:
+                print("You must enter a number!")
+                continue
+        else:
+            break
+
+
+# Main game function
+def num_guess_game():
     random_number = random.randrange(num_1, num_2)
     num_guess = 0
     # While loop for user guesses
-    while condition:
+    while True:
         try:
             guess = int(input("Please enter your guess: "))
         except ValueError:
@@ -48,10 +53,20 @@ while condition:
         elif guess > random_number:
             print("That is incorrect, your guess was too high.")
             num_guess += 1
+
+
+# Start of game
+print("Welcome to the number guessing game.")
+print("Type 2 numbers at the prompt and begin guessing the computer's chosen number.")
+print("Good luck!")
+
+while True:
+    num_selection()
+    num_guess_game()
     # Option to replay the game
     replay = input("Would you like to play again? Y or N \n").lower()
     if replay == "n":
-        condition = False
+        break
     else:
         print("Lets play again!")
 
